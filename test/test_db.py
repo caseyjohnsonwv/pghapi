c5e9117119160bdf6eb173fdb602c20c0c5f3a8b
Johnson <casey.t.johnson@accenture.com>
2020-10-12 09:34:57 -0400

    existing/non-existing tests

--- test/test_db.py
+++ test/test_db.py
@@ -62,11 +62,25 @@ def test_create_user():
     r = client.post(route, json=data)
     assert r.status_code == 200
 
+def test_create_existing_user():
+    setup_database(John())
+    route = Routes.u
+    data = John().to_json()
+    r = client.post(route, json=data)
+    assert r.status_code == 409
+
 def test_create_location():
     route = Routes.l
     data = PPG().to_json()
     r = client.post(route, json=data)
     assert r.status_code == 200
+
+def test_create_existing_location():
+    setup_database(PPG())
+    route = Routes.l
+    data = PPG().to_json()
+    r = client.post(route, json=data)
+    assert r.status_code == 409
 
 def test_create_user_location():
     setup_database(John(), PPG())
@@ -75,24 +89,42 @@ def test_create_user_location():
     r = client.post(route, json=data)
     assert r.status_code == 200
 
+def test_create_existing_user_location():
+    setup_database(John(), PPG())
+    route = Routes.ul
+    # create user location first
+    data = {"phone":John().phone, "address":PPG().address}
+    r = client.post(route, json=data)
+    # then try to create it again
+    r = client.post(route, json=data)
+    assert r.status_code == 409
+
 def test_get_user():
     setup_database(John())
     route = "{route}?phone={phone}".format(route=Routes.u, phone=John().phone)
-    data = John().to_json()
     r = client.get(route)
     r_json = r.json()
     assert r.status_code == 200
     assert r_json["name"] == John().name
     assert r_json["phone"] == John().phone
 
+def test_get_nonexistent_user():
+    route = "{route}?phone={phone}".format(route=Routes.u, phone="000000000")
+    r = client.get(route)
+    assert r.status_code == 404
+
 def test_get_location():
     setup_database(PPG())
     route = "{route}?address={address}".format(route=Routes.l, address=PPG().address)
-    data = PPG().to_json()
     r = client.get(route)
     r_json = r.json()
     assert r.status_code == 200
     assert r_json["address"] == PPG().address
+
+def test_get_nonexistent_location():
+    route = "{route}?address={address}".format(route=Routes.l, address="not a real address")
+    r = client.get(route)
+    assert r.status_code == 404
 
 def test_get_user_location():
     setup_database(John(), PPG())
@@ -105,3 +137,9 @@ def test_get_user_location():
     assert r_json["phone"] == John().phone
     assert r_json["address"] == PPG().address
     assert r_json["nickname"] == ul_nickname
+
+def test_get_nonexistent_user_location():
+    ul_nickname = "Work"
+    route = "{route}?phone={phone}&nickname={nickname}".format(route=Routes.ul, phone=John().phone, nickname=ul_nickname)
+    r = client.get(route)
+    assert r.status_code == 404
